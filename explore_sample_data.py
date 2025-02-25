0dae2f5c5568701c4f280693b37f73cb8cd9f9de
chuck0222 <Calebhuck2001@gmail.com>
2025-02-25 16:57:30 -0500

    Explore sample data file now has new lists for view in Spyder that show summary statistics

--- explore_sample_data.py
+++ explore_sample_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import json
+from collections import Counter
+
 
 # read json file
 with open("xWeather_Erasmus_US_250.json", "r") as file:
@@ -18,11 +20,15 @@ parents = []
 successes = []
 is_alternames = []
 records = []
+events = []
+event_at_location = []
 # parse all locations
 for i in range(0,len(data)):
     records.append(i)
     nest1 = data[i].get('_source', {})
     nested_locations = nest1.get('locations', [])
+    event_type = nest1.get('event')
+    events.append(event_type)
     for location in nested_locations:
         names.append(location.get('name'))
         ids.append(location.get('id'))
@@ -36,6 +42,7 @@ for i in range(0,len(data)):
         parents.append(location.get('parent'))
         successes.append(location.get('success'))
         is_alternames.append(location.get('is_altername'))
+        event_at_location.append(event_type)
 
 locations = pd.DataFrame({
     'name':names,
@@ -47,13 +54,25 @@ locations = pd.DataFrame({
     'score':scores,
     'parent_country':parent_countries,
     'success':successes,
-    'is_altername':is_alternames
+    'is_altername':is_alternames,
+    'event': event_at_location
 })
 
+# unique-ness of locations
 locations.name.drop_duplicates()
 locations.id.drop_duplicates()
 locations.drop_duplicates()
 
-# use coords and separate dataset to map each location to
-# different levels of granularity;
-# region, state, county, etc.
+locations[['lat', 'lon']].value_counts()
+locations[['name', 'lat', 'lon', 'population', 'parent_country', 'id']].drop_duplicates()
+
+# at least in our sample, these fields are consistent for locations across records
+#print(locations[['id', 'name', 'lat', 'lon', 'population', 'parent_country', 'success']].value_counts())
+
+#print(locations)
+
+namecounts = locations[['name']].value_counts()
+countrycounts = locations[['parent_country']].value_counts()
+eventsbylocationseffected = locations[['event']].value_counts()
+weatherbyevent = Counter(events)
+
